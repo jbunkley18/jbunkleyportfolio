b1ba41eb5e7fb3d75664d49091c771b06ef70f04
jbunkley18 <jbunkley18@gmail.com>
2019-11-10 17:34:31 +0800

    Add owlslides and easypiechart.

--- JS/script.js
+++ JS/script.js
@@ -5,10 +5,47 @@ $(document).ready(function() {
 		pagination: false
 	});
 
-	var typed = new Typed('.typed', {
+	let typed = new Typed('.typed', {
 		strings: ['Web Developer'],
 		typeSpeed: 50,
 		startDelay: 500,
 		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items:4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+    		}
+    	}
+	});
+
+	let skillsTopOffset = $(".skillsSection").offset().top;
+
+	$(window).scroll(function(){
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 350){
+			$('.chart').easyPieChart({
+		            easing: 'easeInOut',
+		            barColor: '#fff',
+		            trackColor: false,
+		            scaleColor: false,
+		            lineWidth: 4,
+		            size: 152,
+		            onStep: function(from, to, percent) {
+		            	$(this.el).find('.percent').text(Math.round(percent));
+		            }
+		        });
+		}
 	});
 });
