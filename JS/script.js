8a865047e2c8334419c8984d4417ec4190ca2384
jbunkley18 <jbunkley18@gmail.com>
2019-11-12 11:46:52 +0800

    Try to replace owl carousel with tiny-slider

--- JS/script.js
+++ JS/script.js
@@ -12,24 +12,8 @@ $(document).ready(function() {
 		showCursor: false
 	});
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items:4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-    		}
-    	}
-	});
+
+	let slider = new ti
 
 	let skillsTopOffset = $(".skillsSection").offset().top;
 
