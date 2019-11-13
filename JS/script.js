2f1e100807563c3430ac02920ccb73c83e3c6217
jbunkley18 <jbunkley18@gmail.com>
2019-11-13 09:50:39 +0800

    Go back to owl carousel for now

--- JS/script.js
+++ JS/script.js
@@ -12,8 +12,24 @@ $(document).ready(function() {
 		showCursor: false
 	});
 
-
-	let slider = new ti
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items:4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+    		}
+    	}
+	});
 
 	let skillsTopOffset = $(".skillsSection").offset().top;
 
